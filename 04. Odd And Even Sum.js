3cb249387b0d8a4a643a67e391db92750155e5ad
Anita-dev-hub <anitaboncheva01@gmail.com>
2025-06-09 13:52:47 +0300

    Ex- function- Odd And Even Sum

---
+++ 04. Odd And Even Sum.js
@@ -0,0 +1,40 @@
+function solve(n){
+    function extractEven(num){
+        let result = [];
+        do{
+            let lastNumber = num % 10;
+            if(lastNumber % 2 === 0){
+                result.push(lastNumber);
+            }
+            num = parseInt(num / 10);
+        } while(num > 0)
+        return result;
+    }
+    
+    function extractOdd(num){
+        let result = [];
+        do{
+            let lastNumber = num % 10;
+            if(lastNumber % 2 > 0){
+                result.push(lastNumber);
+            }
+            num = parseInt(num / 10);
+        } while(num > 0)
+        return result;
+    }
+
+    function sumArrays(arr){
+        let result = 0;
+        for (let i = 0; i < arr.length; i++){
+            result += arr[i];
+        }
+        return result;
+    }
+    
+    let odd = extractOdd(n);
+    let even = extractEven(n);
+    let oddSum = sumArrays(odd);
+    let evenSum = sumArrays(even);
+    console.log(`Odd sum = ${oddSum}, Even sum = ${evenSum}`);
+}
+solve(3495892137259234)
